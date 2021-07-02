26219b2232fa274ddff7d15e67c344531657ad1b
chirag1712 <chiragjindal1712@gmail.com>
2021-07-03 00:09:25 +0530

    added backend structure for user entity and user authentication endpoints along with backend MVC pattern

---
+++ backend/app/models/user.model.js
@@ -0,0 +1,35 @@
+const sql = require("./db.js");
+
+const User = function (user) {
+    this.user_name = user.user_name;
+    this.email = user.email;
+    this.hashed_password = user.hashed_password;
+};
+
+User.createUser = newUser => {
+    return new Promise((resolve, reject) => {
+        sql.query("INSERT INTO User SET ?", newUser, (err, res) => {
+            if (err) {
+                console.log("error: ", err);
+                reject(err);
+            }
+            console.log("user created: ", { user_id: res.insertId, ...newUser });
+            resolve(res.insertId);
+        });
+    });
+}
+
+User.findUser = email => {
+    return new Promise((resolve, reject) => {
+        sql.query("SELECT * FROM User WHERE email = ?", email, (err, res) => {
+            if (err) {
+                console.log("error: ", err);
+                reject(err);
+            }
+            console.log("user found: ", res);
+            resolve(res[0]);
+        });
+    });
+}
+
+module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-// server file
+const express = require('express');
+const app = express();
+app.use(express.json({ extended: false }));
+
+// Welcome backend route
+app.get("/", (_, res) => {
+    res.json({ message: "Billify backend." });
+});
+
+// all other routes here
+app.use("/api/users", require("./app/routes/user.routes.js"));
+
+// server setup
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
