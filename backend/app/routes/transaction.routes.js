fe90b9f7c5c114a465ff12a38b76ea2be27fe4b3
pranavpsv <pranav.vadrevu363@gmail.com>
2021-07-06 19:12:52 +0200

    parse receipt

--- backend/app/routes/transaction.routes.js
+++ backend/app/routes/transaction.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const transaction = require("../controllers/transaction.controller.js");
 
-// @route POST api/transactions/parse_receipt
+// @route POST api/transactions/parse-receipt
 router.post("/parse-receipt", transaction.parse_receipt);
 
 module.exports = router;
