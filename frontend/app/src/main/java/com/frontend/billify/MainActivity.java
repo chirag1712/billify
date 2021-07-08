1efa8b861aeacc84b4b75ee12d9802dbee4941f4
Denis <denis.o.shapiro@gmail.com>
2021-07-08 00:27:50 -0400

    add menu

--- frontend/app/src/main/java/com/frontend/billify/MainActivity.java
+++ frontend/app/src/main/java/com/frontend/billify/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.FragmentTransaction;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 import android.widget.Button;
@@ -57,5 +59,12 @@ public class MainActivity extends AppCompatActivity implements NavigationHost {
         }
 
         transaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
