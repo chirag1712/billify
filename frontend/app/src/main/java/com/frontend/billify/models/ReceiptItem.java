c20c14e123f7117a6fcb0bb2554dd004decc69dc
chirag1712 <chiragjindal1712@gmail.com>
2021-07-27 02:31:28 +0530

    inital commit - grouped all activities together in a package, renamed fragments package and moved some models to the corresponding data models package

---
+++ frontend/app/src/main/java/com/frontend/billify/models/ReceiptItem.java
@@ -0,0 +1,35 @@
+package com.frontend.billify.models;
+
+public class ReceiptItem {
+    private String name;
+    private double price;
+
+    public ReceiptItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return Double.toString(price);
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "ReceiptItem{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
