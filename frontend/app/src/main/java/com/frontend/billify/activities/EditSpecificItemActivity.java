1cfa08cda7788b86286edeb4c1eec576295dac60
pranavpsv <pranav.vadrevu363@gmail.com>
2021-07-27 23:30:55 +0200

    Add Swipe to Delete icon

--- frontend/app/src/main/java/com/frontend/billify/activities/EditSpecificItemActivity.java
+++ frontend/app/src/main/java/com/frontend/billify/activities/EditSpecificItemActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,6 +15,7 @@ public class EditSpecificItemActivity extends AppCompatActivity {
 
     TextView itemNameView, itemPriceView;
     String itemName, itemPrice;
+    int itemIndex;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +25,17 @@ public class EditSpecificItemActivity extends AppCompatActivity {
 
         getData();
         setData();
+
+        Button saveEditButton = findViewById(R.id.save_edit_specific_item_button);
+
+        saveEditButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent = new Intent(EditSpecificItemActivity.this, EditItemsActivity.class);
+//                intent.
+//                startActivity(intent);
+            }
+        });
     }
 
     private void getData() {
@@ -29,6 +43,8 @@ public class EditSpecificItemActivity extends AppCompatActivity {
         if (intent.hasExtra("item_name") && intent.hasExtra("item_price")) {
             itemName = intent.getStringExtra("item_name");
             itemPrice = intent.getStringExtra("item_price");
+            itemIndex = intent.getIntExtra("item_index", -1);
+
         } else {
             Toast.makeText(this, "Items not found", Toast.LENGTH_SHORT).show();
         }
