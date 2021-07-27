a3979c4fbdf8e75607f37a88b8905fc5ecc4620c
pranavpsv <pranav.vadrevu363@gmail.com>
2021-07-27 15:42:58 +0200

    Replace onActivityResult with ActivityResultLauncher

--- frontend/app/src/main/java/com/frontend/billify/activities/UploadReceiptActivity.java
+++ frontend/app/src/main/java/com/frontend/billify/activities/UploadReceiptActivity.java
@@ -1,6 +1,7 @@
 package com.frontend.billify.activities;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -13,6 +14,10 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -45,6 +50,9 @@ public class UploadReceiptActivity extends AppCompatActivity {
 
     private ProgressBar uploadProgress;
 
+    ActivityResultLauncher<Intent> cameraResultLauncher;
+
+    ActivityResultLauncher<Intent> galleryResultLauncher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -75,10 +83,7 @@ public class UploadReceiptActivity extends AppCompatActivity {
                             if (photoFile != null) {
                                 Uri photoURI = UploadReceiptActivity.this.getPhotoURI(photoFile);
                                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                                UploadReceiptActivity.this.startActivityForResult(
-                                        takePictureIntent,
-                                        CAMERA_PIC_REQUEST
-                                );
+                                cameraResultLauncher.launch(takePictureIntent);
                                 UploadReceiptActivity.this.currPhotoFile = photoFile;
                             }
                         }
@@ -101,18 +106,64 @@ public class UploadReceiptActivity extends AppCompatActivity {
                             MediaStore.Images.Media.INTERNAL_CONTENT_URI);
 
                     if (imagePickerIntent.resolveActivity(getPackageManager()) != null) {
-                        startActivityForResult(imagePickerIntent, IMAGE_PICKER_CODE);
+                        galleryResultLauncher.launch(imagePickerIntent);
                     }
-//                    Intent imagePickerIntent = new Intent(Intent.ACTION_GET_CONTENT);
-                    imagePickerIntent.setType("image/*");
-//
-//                    startActivityForResult(Intent.createChooser(imagePickerIntent, "Select Picture"),
-//                            IMAGE_PICKER_CODE);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         });
+
+        cameraResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            try {
+                                uploadProgress.setVisibility(View.VISIBLE);
+                                System.out.println("In upload image " + getIntent().getStringExtra("gid"));
+                                createTransaction(Integer.parseInt(getIntent().getStringExtra("gid")), transactionController);
+                            } catch (Exception e) {
+                                Log.d(TAG, "onActivityResult: " + e.toString());
+                            }
+                        }
+                    }
+                }
+        );
+
+        galleryResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            try {
+                                // Creating file
+                                currPhotoFile = null;
+                                try {
+                                    currPhotoFile = createImageFile();
+                                } catch (IOException ex) {
+                                    Log.d(TAG, "Error occurred while creating the file");
+                                }
+                                Intent data = result.getData();
+                                InputStream inputStream = getContentResolver().openInputStream(data.getData());
+                                FileOutputStream outputStream = new FileOutputStream(currPhotoFile);
+                                // Copying
+                                copyStream(inputStream, outputStream);
+                                outputStream.close();
+                                inputStream.close();
+                                uploadProgress.setVisibility(View.VISIBLE);
+                                System.out.println("In upload image " + getIntent().getStringExtra("gid"));
+                                createTransaction(Integer.parseInt(getIntent().getStringExtra("gid")), transactionController);
+                            } catch (Exception e) {
+                                Log.d(TAG, "onActivityResult: " + e.toString());
+                            }
+                        }
+                    }
+                }
+        );
+
     }
 
     private Uri getPhotoURI(File photoFile) {
@@ -170,6 +221,7 @@ public class UploadReceiptActivity extends AppCompatActivity {
             }
         });
     }
+
     private void requestCameraPermission() {
         System.out.println("Ask permission!");
         ActivityCompat.requestPermissions(this,
@@ -194,44 +246,8 @@ public class UploadReceiptActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        try {
-            if ((requestCode == CAMERA_PIC_REQUEST) && (resultCode == RESULT_OK)) {
-                this.uploadProgress.setVisibility(View.VISIBLE);
-                System.out.println("In upload image " + getIntent().getStringExtra("gid"));
-                this.createTransaction(Integer.parseInt(getIntent().getStringExtra("gid")), transactionController);
-            } else if ((requestCode == IMAGE_PICKER_CODE) && (resultCode == RESULT_OK)) {
-                try {
-                    // Creating file
-                    this.currPhotoFile = null;
-                    try {
-                        this.currPhotoFile = createImageFile();
-                    } catch (IOException ex) {
-                        Log.d(TAG, "Error occurred while creating the file");
-                    }
 
-                    InputStream inputStream = getContentResolver().openInputStream(data.getData());
-                    FileOutputStream outputStream = new FileOutputStream(this.currPhotoFile);
-                    // Copying
-                    copyStream(inputStream, outputStream);
-                    outputStream.close();
-                    inputStream.close();
-                    this.uploadProgress.setVisibility(View.VISIBLE);
-                    System.out.println("In upload image " + getIntent().getStringExtra("gid"));
-                    this.createTransaction(Integer.parseInt(getIntent().getStringExtra("gid")), transactionController);
-                } catch (Exception e) {
-                    Log.d(TAG, "onActivityResult: " + e.toString());
-                }
 
-            }
-        } catch (Exception ex) {
-            Toast.makeText(this, ex.toString(),
-                    Toast.LENGTH_SHORT).show();
-        }
-
-    }
 
     private void copyStream(InputStream inputStream, FileOutputStream outputStream) throws IOException {
         byte[] buffer = new byte[1024];
