09bd3bd6b815e199267405df5d39630c33fb8cb5
pranavpsv <pranav.vadrevu363@gmail.com>
2021-07-28 03:18:25 +0200

    Add swipe to delete on UI (doesn't actually delete item yet)

--- frontend/app/src/main/java/com/frontend/billify/activities/EditItemsActivity.java
+++ frontend/app/src/main/java/com/frontend/billify/activities/EditItemsActivity.java
@@ -6,6 +6,8 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.view.menu.MenuView;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -31,6 +33,8 @@ import com.frontend.billify.models.Transaction;
 import com.frontend.billify.services.RetrofitService;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import android.os.Bundle;
+import android.view.View;
+
 import com.frontend.billify.R;
 import com.frontend.billify.adapters.EditItemsRecViewAdapter;
 
@@ -287,6 +291,40 @@ public class EditItemsActivity extends AppCompatActivity {
         public void onChildDraw(@NonNull @NotNull Canvas c, @NonNull @NotNull RecyclerView recyclerView,
                                 @NonNull @NotNull RecyclerView.ViewHolder viewHolder,
                                 float dX, float dY, int actionState, boolean isCurrentlyActive) {
+
+            // Code adapted from https://github.com/kitek/android-rv-swipe-delete/blob/master/app/src/main/java/pl/kitek/rvswipetodelete/SwipeToDeleteCallback.kt
+            ColorDrawable background = new ColorDrawable();
+
+            Drawable deleteIcon = ContextCompat.getDrawable(
+                    EditItemsActivity.this,
+                    R.drawable.ic_baseline_delete_24
+            );
+            int inHeight = deleteIcon.getIntrinsicHeight();
+            int inWidth = deleteIcon.getIntrinsicWidth();
+
+            View itemView = viewHolder.itemView;
+            float itemHeight = itemView.getBottom() - itemView.getTop();
+            int backgroundColor = Color.parseColor("#FF0000");
+            background.setColor(backgroundColor);
+            background.setBounds(itemView.getRight() + (int) dX, itemView.getTop(), itemView.getRight(), itemView.getBottom());
+            background.draw(c);
+
+
+            // Calculate position of delete icon
+            int iconTop = (int) (itemView.getTop() + (itemHeight - inHeight) / 2);
+            int iconMargin = (int) ((itemHeight - inHeight) / 2);
+            int iconLeft = itemView.getRight() - iconMargin - inWidth;
+            int iconRight = itemView.getRight() - iconMargin;
+            int iconBottom = iconTop + inHeight;
+
+            deleteIcon.setBounds(iconLeft, iconTop, iconRight, iconBottom);
+            deleteIcon.draw(c);
+
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
+
+        @Override
+        public void onChildDraw(@NonNull @NotNull Canvas c, @NonNull @NotNull RecyclerView recyclerView, @NonNull @NotNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
 
             // Code adapted from https://github.com/kitek/android-rv-swipe-delete/blob/master/app/src/main/java/pl/kitek/rvswipetodelete/SwipeToDeleteCallback.kt
             ColorDrawable background = new ColorDrawable();
