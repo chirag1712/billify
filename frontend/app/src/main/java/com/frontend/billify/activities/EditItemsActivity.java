491243d37e4fd14b6e6e29e129151f1d23bf2e7d
pranavpsv <pranav.vadrevu363@gmail.com>
2021-07-28 03:18:25 +0200

    Add swipe to delete on UI (doesn't actually delete item yet)

--- frontend/app/src/main/java/com/frontend/billify/activities/EditItemsActivity.java
+++ frontend/app/src/main/java/com/frontend/billify/activities/EditItemsActivity.java
@@ -2,11 +2,21 @@ package com.frontend.billify.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.view.menu.MenuView;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+
 import com.frontend.billify.R;
 import com.frontend.billify.adapters.EditItemsRecViewAdapter;
 
@@ -38,6 +48,7 @@ public class EditItemsActivity extends AppCompatActivity {
     }
 
     ItemTouchHelper.SimpleCallback swipeDeleteCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
         @Override
         public boolean onMove(@NonNull @NotNull RecyclerView recyclerView,
                               @NonNull @NotNull RecyclerView.ViewHolder viewHolder,
@@ -45,10 +56,46 @@ public class EditItemsActivity extends AppCompatActivity {
             return false;
         }
 
+        // NOTE: Swipe triggers delete
         @Override
         public void onSwiped(@NonNull @NotNull RecyclerView.ViewHolder viewHolder, int i) {
             items.remove(viewHolder.getBindingAdapterPosition());
             editItemsAdapter.notifyDataSetChanged();
         }
+
+        @Override
+        public void onChildDraw(@NonNull @NotNull Canvas c, @NonNull @NotNull RecyclerView recyclerView, @NonNull @NotNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+
+            // Code adapted from https://github.com/kitek/android-rv-swipe-delete/blob/master/app/src/main/java/pl/kitek/rvswipetodelete/SwipeToDeleteCallback.kt
+            ColorDrawable background = new ColorDrawable();
+
+            Drawable deleteIcon = ContextCompat.getDrawable(
+                    EditItemsActivity.this,
+                    R.drawable.ic_baseline_delete_24
+            );
+            int inHeight = deleteIcon.getIntrinsicHeight();
+            int inWidth = deleteIcon.getIntrinsicWidth();
+
+            View itemView = viewHolder.itemView;
+            float itemHeight = itemView.getBottom() - itemView.getTop();
+            int backgroundColor = Color.parseColor("#FF0000");
+            background.setColor(backgroundColor);
+            background.setBounds(itemView.getRight() + (int) dX, itemView.getTop(), itemView.getRight(), itemView.getBottom());
+            background.draw(c);
+
+
+            // Calculate position of delete icon
+            int iconTop = (int) (itemView.getTop() + (itemHeight - inHeight) / 2);
+            int iconMargin = (int) ((itemHeight - inHeight) / 2);
+            int iconLeft = itemView.getRight() - iconMargin - inWidth;
+            int iconRight = itemView.getRight() - iconMargin;
+            int iconBottom = iconTop + inHeight;
+
+            deleteIcon.setBounds(iconLeft, iconTop, iconRight, iconBottom);
+            deleteIcon.draw(c);
+
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
     };
+
 }
