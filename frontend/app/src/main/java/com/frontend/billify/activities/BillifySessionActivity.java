9abe8a283bdc9ab2c005559a8bba8d3d73f59bd2
Chirag <cjindal@uwaterloo.ca>
2021-08-06 15:24:52 -0400

    added IP of school server to mobile app

--- frontend/app/src/main/java/com/frontend/billify/activities/BillifySessionActivity.java
+++ frontend/app/src/main/java/com/frontend/billify/activities/BillifySessionActivity.java
@@ -25,7 +25,7 @@ public class BillifySessionActivity extends Activity {
 
     {
         try {
-            mSocket = IO.socket("http://10.0.2.2:5000");
+            mSocket = IO.socket("http://129.97.167.52:5000");
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }
