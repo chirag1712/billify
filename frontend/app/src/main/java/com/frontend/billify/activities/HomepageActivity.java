5efb87a681c997ff463c618161e48b68ff2e52af
chirag1712 <chiragjindal1712@gmail.com>
2021-08-06 22:15:19 +0530

    removed the hard coded billify session from the home screen

--- frontend/app/src/main/java/com/frontend/billify/activities/HomepageActivity.java
+++ frontend/app/src/main/java/com/frontend/billify/activities/HomepageActivity.java
@@ -4,20 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.frontend.billify.R;
 import com.frontend.billify.activities.view_transactions.ViewTransactionsActivity;
 import com.frontend.billify.controllers.TransactionController;
-import com.frontend.billify.models.Transaction;
 import com.frontend.billify.persistence.Persistence;
 import com.frontend.billify.services.RetrofitService;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class HomepageActivity extends AppCompatActivity {
     @Override
@@ -64,45 +58,6 @@ public class HomepageActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(HomepageActivity.this, ViewTransactionsActivity.class);
                 startActivity(intent);
-            }
-        });
-            
-        join_billify_session.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                transactionController.getTransaction(280).enqueue(new Callback<Transaction>() {
-                    @Override
-                    public void onResponse(Call<Transaction> call, Response<Transaction> response) {
-                        Transaction currTransaction = new Transaction(response.body());
-                        System.out.println(
-                                "Successful homepage join billify session request with return value: "
-                                + currTransaction.getName()
-                        );
-                        currTransaction.printItems();
-                        System.out.println("Curr Transaction TID: " +
-                                String.valueOf(currTransaction.getTid()));
-                        Intent moveToItemizedScreenIntent = new Intent(
-                                HomepageActivity.this,
-                                ItemizedViewActivity.class
-                        );
-                        Bundle transactionBundle = new Bundle();
-                        transactionBundle.putSerializable("SerializedTransaction", currTransaction);
-                        moveToItemizedScreenIntent.putExtra(
-                                "TransactionBundle",
-                                transactionBundle
-                        );
-                        startActivity(moveToItemizedScreenIntent);
-
-                    }
-
-                    @Override
-                    public void onFailure(Call<Transaction> call, Throwable t) {
-                        Toast.makeText(HomepageActivity.this,
-                                "Failed joining the billify session",
-                                Toast.LENGTH_SHORT).show();
-                        t.printStackTrace();
-                    }
-                });
             }
         });
 
